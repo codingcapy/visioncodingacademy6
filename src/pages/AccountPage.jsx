dac33c43dc1c2ff8a2652964e82276f40f7afa3c
Paul K <spkim0921@gmail.com>
2024-08-14 12:24:31 -0700

    59th commit
    implementing edit password in AccountPage.jsx component

--- src/pages/AccountPage.jsx
+++ src/pages/AccountPage.jsx
@@ -1,13 +1,17 @@
 import { useState } from "react"
 import useAuthStore from "../store/AuthStore"
 import visionCodingIcon from "/yellow1.png"
+import axios from "axios"
+import DOMAIN from "../services/endpoint"
+import { useNavigate } from "react-router-dom"
 
 export default function AccountPage() {
 
-    const { user } = useAuthStore((state) => state)
+    const { user, logoutService } = useAuthStore((state) => state)
     const [editPasswordMode, setEditPasswordMode] = useState(false)
     const [editUsernameMode, setEditUsernameMode] = useState(false)
     const [notification, setNotification] = useState("");
+    const navigate = useNavigate();
 
     async function handleEditPassword(e) {
         e.preventDefault()
@@ -33,16 +37,18 @@ export default function AccountPage() {
         e.preventDefault()
         const username = e.target.username.value;
         const userId = user.user_id
-        const updatedUsername = username;
-        try {
-            const res = await axios.post(`${DOMAIN}/api/users/update/${userId}`, updatedUsername);
-            if (res?.data.success) {
-                setEditUsernameMode(false);
-                setMessage("Username updated successfully!")
-            }
+        const updatedUsername = { username };
+        const res = await axios.post(`${DOMAIN}/api/users/update/${userId}`, updatedUsername);
+        if (res?.data.success) {
+            setEditUsernameMode(false);
+            setNotification("Username updated successfully! Logging out...")
+            setTimeout(() => {
+                navigate("/")
+                logoutService()
+            }, 1000)
         }
-        catch (err) {
-            setNotification("There was an error updating the password :( We will look into this issue, please try again in the near future!")
+        else {
+            setNotification(res?.data.message)
         }
     }
 
@@ -59,13 +65,14 @@ export default function AccountPage() {
             <section className="flex flex-col mx-auto">
                 <div className="mx-auto pb-10">
                     <h2 className="text-2xl">Username: <span className="text-yellow-200">{user.username}</span></h2>
-                    {editUsernameMode ?
+                    {/* {editUsernameMode ?
                         <form onSubmit={handleEditUsername} className="flex flex-col">
                             <input type="text" id="username" name="username" placeholder="New Username" required className="px-2 border rounded-lg border-slate-700 py-1 text-black" />
+                            <p className="pt-2">Warning: You will be logged out once you change your username!</p>
                             <button type="submit" className="rounded-xl my-5 py-2 px-2 bg-slate-700 text-white" >Change Username</button>
                             <button className="" onClick={() => setEditUsernameMode(false)}>Cancel</button>
                         </form>
-                        : <button className="px-3 py-3 my-7 border-4 rounded border-yellow-300 text-yellow-300 font-bold md:text-2xl hover:bg-yellow-300 hover:text-black transition-all ease duration-500 w-[200px] mx-auto" onClick={() => setEditUsernameMode(true)}>Edit</button>}
+                        : <button className="px-3 py-3 my-7 border-4 rounded border-yellow-300 text-yellow-300 font-bold md:text-2xl hover:bg-yellow-300 hover:text-black transition-all ease duration-500 w-[200px] mx-auto" onClick={() => setEditUsernameMode(true)}>Edit</button>} */}
                 </div>
                 <div className="mx-auto">
                     <h2 className="text-2xl">Password: <span className="text-yellow-200">••••••••••</span></h2>
@@ -79,6 +86,7 @@ export default function AccountPage() {
                         <button className="px-3 py-3 my-7 border-4 rounded border-yellow-300 text-yellow-300 font-bold md:text-2xl hover:bg-yellow-300 hover:text-black transition-all ease duration-500 w-[200px] mx-auto" onClick={() => setEditPasswordMode(true)}>Edit</button>}
                 </div>
             </section>
+            <p className="text-center">{notification}</p>
             <div className="mt-20">
                 <div
                     className="static bg-gradient-to-r from-black via-yellow-400 to-black h-[2px]">
