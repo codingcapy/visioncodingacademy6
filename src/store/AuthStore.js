2115106e00db506258c0b809224136d71669bd67
Paul K <spkim0921@gmail.com>
2024-08-13 17:05:07 -0700

    49th commit
    removing tests on loginService in AuthStore.js

--- src/store/AuthStore.js
+++ src/store/AuthStore.js
@@ -21,8 +21,6 @@ const useAuthStore = create((set, get) => ({
         set({ user: null, authLoading: false, tokenLoading: false });
     },
     loginService: async (email, password) => {
-        console.log(email)
-        console.log(password)
         set({ authLoading: true });
         try {
             const res = await axios.post(`${DOMAIN}/api/user/login`, { email, password });
